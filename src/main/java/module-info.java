3e7d4fac70580854e099a17fe76529d63d3c6a7c
pere <pere@ddcc7e996a23>
2022-10-07 13:27:47 +0000

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * The testspies module provides common spies used when testing Cora projects.
+ */
+module se.uu.ub.cora.testspies {
+
+	requires org.testng;
+	requires se.uu.ub.cora.httphandler;
+	requires transitive se.uu.ub.cora.testutils;
+
+	exports se.uu.ub.cora.testspies.httphandler;
+}
